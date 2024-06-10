cb09b489b6417ad147745a0482c9a014073a599c
Miragitai <Ukolov2016@gmail.com>
2024-06-10 16:12:08 +0300

    fix 2?

--- storage_display.lua
+++ storage_display.lua
@@ -35,7 +35,6 @@ local menubar = monitorFrame:addMenubar():setScrollable() -- we create a menubar
     :addItem("Home")
     :addItem("Info")
     :addItem("Null")
-    :openSubFrame(2)
 
 ----------------------------------------------------------------------------------------------------------------------
 -- Main frame - [1]
